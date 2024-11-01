c733c2cc747f391712b0e8963854b939e0739db2
shagzheevandy <shaghzheevandy@gmail.com>
2024-11-01 23:20:09 +0800

    push

---
+++ Peremennye.py
@@ -0,0 +1,5 @@
+amount_of_homework = 12
+spent_hours = 1.5
+name_of_course = 'Python'
+required_time_per_a_task = spent_hours/amount_of_homework
+print('Курс:',name_of_course,',','всего задач:', amount_of_homework,',', 'затрачено часов:',spent_hours, ',','среднее время выполнения:', required_time_per_a_task,'часа')

2a3e4f2d6db18647657342ffe799ea3e9b8ef859
shagzheevandy <shaghzheevandy@gmail.com>
2024-11-01 23:20:09 +0800

    push

--- main.py
+++ main.py
@@ -3,17 +3,9 @@ print(9**0.5*5)
 # 2nd program
 print(9.99>9.98 and 1000!=1000.1)
 # 3rd program
+print(2*2+2)
+print(2*(2+2))
 print(2*2+2==2*(2+2))
 # 4th program
 print(int(123.456*10%10))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('World')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/

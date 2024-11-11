263cc19b6bee100ddb59b71127adcd0ce7455b83
shagzheevandy <shagzheevandy@gmail.com>
2024-11-11 17:19:02 +0800

    push

--- module_1_6.py
+++ module_1_6.py
@@ -6,8 +6,9 @@ my_dict['Slava'] = 1992
 print(my_dict['Slava'])
 my_dict['Igor'] = 1993
 print(my_dict['Igor'])
-del my_dict['Andy']
-print(my_dict.get('Andy'))
+print(my_dict)
+a = my_dict.pop('Andy')
+print(a)
 print(my_dict)
 
 my_set = {1,1,2,2,2,3,3,3,3,'one','two','two','three', True, False}
